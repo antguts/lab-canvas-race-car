673ffd0a8bc0d50bcbc8d7ee0a94f2c040fb241d
antguts <antgut035@gmail.com>
2020-06-18 17:40:42 -0400

    Added collision

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+//canvas
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
+//road
+const roadImage = new Image() 
+roadImage.src = 'images/road.png'
+//car
+const carImage = new Image() 
+carImage.src = 'images/car.png'
+
 
 document.getElementById('start-button').onclick = () => {
   startGame();
@@ -17,61 +25,57 @@ function startGame() {
 //Car obj
 let carObj = {
 // Left most point is 80 and right most point is 380
-  leftRight: 150,
+  x: 150,
+  y: 600,
+  width:65,
+  height:100,
   goLeft: function(){
-    if(this.leftRight>=80)
-      this.leftRight-=5;
+    if(this.x>=80)
+      this.x-=7;
   },
   goRight: function(){
-    if(this.leftRight<=380)
-      this.leftRight+=5;
+    if(this.x<=380)
+      this.x+=7;
   }
 }
 
 //Obst obj
 let obst={
   obs1: {
-    width: 0,
-    height: 0,
-    side: 80
+    width: 200,
+    height:50,
+    y: 10,
+    x: 80
   },
   obs2: {
-    width: 0,
-    height: -290,
-    side: 80
+    width: 30,
+    height:50,
+    y: -300,
+    x: 80
   },
   obs3: {
-    width: 0,
-    height: -580,
-    side: 200
+    width: 10,
+    height:50,
+    y: -590,
+    x: 200
   }
 }
 
 function obsticles(){
   ctx.fillStyle='red'
   for(property in obst){
-    let curObj=obst[property]
-    
-    if(obst[property].height>=855){ 
-      curObj.height=0
-      curObj.width=Math.floor(Math.random()* (270 - 80) + 80)
-      ctx.fillRect(curObj.side,curObj.height,curObj.width,40)
+    let curObj=obst[property]    
+    if(curObj.y>=855){ 
+      curObj.y=0
+      curObj.width=Math.floor(Math.random()* (230 - 80) + 80)
+      ctx.fillRect(curObj.x,curObj.y,curObj.width,curObj.height)
     } else {
-      curObj.height+=3
-      ctx.fillRect(curObj.side,curObj.height,curObj.width,40)
+      curObj.y+=3
+      ctx.fillRect(curObj.x,curObj.y,curObj.width,curObj.height)
     }
   }
- 
-  
-  // ctx.fillRect(80,obst.obs2.height,obst.obs2.width,40)
-  // ctx.fillRect(80,obst.obs3.height,obst.obs3.width,40)
-  // ctx.fillRect(80,obst.obs4.height,obst.obs4.width,40)
-  // ctx.fillRect(80,obst.obs5.height,obst.obs5.width,40)
-
-
 }
 
-// ctx.drawImage(carObj.leftRight,600,65,100)
 
 
 
@@ -82,42 +86,39 @@ function keyInput(e){
   if(keyCode == 37){//left arrow
     // setInterval(carObj.goLeft(),100)
     carObj.goLeft()
-    console.log("left: ",carObj.leftRight)
+    console.log("left: ",carObj.x)
   }
-  
   if(keyCode == 39){//right arrow
     carObj.goRight()
-    console.log("right: ",carObj.leftRight)
+    console.log("right: ",carObj.x)
   }
-  // updateBoard()
 };
 
 function updateBoard(){
-    //road
-    let roadImage = new Image() 
-    roadImage.src = 'images/road.png'
-    roadImage.onload = function(e){
-      ctx.drawImage(roadImage,10,10,500,1000)
-    }
-  
-    //car
-    let carImage = new Image() 
-    carImage.src = 'images/car.png'
-    carImage.onload = function(e){
-      ctx.drawImage(carImage,carObj.leftRight,600,65,100)
-    }
-
-
+  ctx.drawImage(roadImage,10,10,500,1500)
+  ctx.drawImage(carImage,carObj.x,carObj.y,carObj.width,carObj.height)
 }
 
+function checkCollision(){
 
+  for(property in obst){
+    if (obst[property].x < carObj.x + carObj.width &&
+      obst[property].x + obst[property].width > carObj.x &&
+      obst[property].y < carObj.y + carObj.height &&
+      obst[property].y + obst[property].height > carObj.y) {
+          // collision detected!
+          alert('Collision detected: Game Over')
+          window.cancelAnimationFrame(id)
+    }//end if
+  }//end for
+}//end function
 
-// let obsticles=[]
-
+let id=null
 function animate(){
-  window.requestAnimationFrame(animate)
+  id=window.requestAnimationFrame(animate)
   // obsticles.push(ctx.fillRect(80,0,100,40))
   // ctx.clesarRect(0,0,canvas.width,canvas.height)
-  obsticles()
   updateBoard()
+  obsticles()
+  checkCollision()
 }
